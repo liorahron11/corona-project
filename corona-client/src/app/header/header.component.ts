19409400e8967ffa4a25454ab336d1d93a5e92bf
ליאור אהרון <liorahron11.gmail.com>
2020-12-09 11:29:32 +0200

    feat(): add headers and improve css and flex

--- corona-client/src/app/header/header.component.ts
+++ corona-client/src/app/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { faShieldVirus } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit, Input } from '@angular/core';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +7,8 @@ import { faShieldVirus } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  virusIcon = faShieldVirus;
+  @Input() title: string;
+  @Input() icon: IconDefinition;
 
   constructor() {}
 
