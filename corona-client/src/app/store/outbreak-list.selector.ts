db3ef6103365f5a45a327c46c0684456e9917166
ליאור אהרון <liorahron11.gmail.com>
2021-01-05 10:15:38 +0200

    fix(): remove unused selector

--- corona-client/src/app/store/outbreak-list.selector.ts
+++ corona-client/src/app/store/outbreak-list.selector.ts
@@ -1,11 +1,6 @@
 import { createSelector } from '@ngrx/store';
 import { State } from './outbreak-list.reducer';
 
-export const selectState = createSelector(
-  (state: { storeState: State }) => state.storeState,
-  (state) => state
-);
-
 export const selectMapItemsList = createSelector(
   (state: { storeState: State }) => state.storeState,
   (state) => state.mapItemsList
